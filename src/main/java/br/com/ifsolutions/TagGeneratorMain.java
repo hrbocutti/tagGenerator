ef00d5f4ae995d25425135e06576ffab645b7be3
Higor Bocutti <webmaster@polyhousestore.com>
2018-01-04 23:38:19 -0200

    New Branch

---
+++ src/main/java/br/com/ifsolutions/TagGeneratorMain.java
@@ -0,0 +1,20 @@
+package br.com.ifsolutions;
+
+import br.com.ifsolutions.controller.SQLiteController;
+import br.com.ifsolutions.controller.TagController;
+
+public class TagGeneratorMain {
+    public static void main(String[] args) {
+        //try to access sqllite
+
+        Boolean baseExist;
+        SQLiteController sqlite = new SQLiteController();
+        //String sqlitePath = "C:\\tagGenerator\\tagGenerator.sqlite";
+        String sqlitePath = "c:/tagGenerator/tagGenerator.sqlite";
+        sqlite.checkIfHaveSqliteBase(sqlitePath);
+        System.out.println("Entrou aqui");
+        //Opções de Impressão e busca
+        TagController tagController = new TagController();
+        tagController.renderTagMenu();
+    }
+}
